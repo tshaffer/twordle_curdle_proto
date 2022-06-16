23e05b3014e9159745fe8ad63caaef2e4c6418ff
Ted Shaffer <shaffer.family@gmail.com>
2022-06-16 06:38:03 -0700

    Removed most remnants of 'color' code.

--- popup.js
+++ popup.js
@@ -1,106 +1,79 @@
 const serverUrl = 'http://localhost:8000';
 const versionUrl = serverUrl + '/api/v1/version';
 
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+fetch(versionUrl)
+  .then(response => response.text())
+  .then(response => console.log(response));
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+chrome.tabs.query({ active: true, currentWindow: true })
+  .then(([tab]) => {
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async (event) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: executeContentScript,
+    });
 
-  console.log(event);
+    chrome.runtime.onMessage.addListener(
+      function (request) {
+        console.log('extension onMessage.addListener invoked, received:');
+        console.log(request);
+      }
+    );
 
-  fetch(versionUrl)
-    .then(response => response.text())
-    .then(response => flibbet(response));
-
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-chrome.runtime.onMessage.addListener(
-  function (request) {
-    console.log('extension onMessage.addListener invoked, received:');
-    console.log(request);
   }
-);
-
-function flibbet(response) {
-  console.log('response to getVersion');
-  console.log(response);
-}
-
+  );
 
 // CONTENT SCRIPT
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+function executeContentScript() {
 
-    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
-    // https://stackoverflow.com/questions/2257070/detect-numbers-or-letters-with-jquery-javascript
+  // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+  // https://stackoverflow.com/questions/2257070/detect-numbers-or-letters-with-jquery-javascript
 
-    document.body.addEventListener("keydown", async (event) => {
+  document.body.addEventListener("keydown", async (event) => {
 
-      console.log('content script keydown handler invoked');
+    console.log('content script keydown handler invoked');
 
-      console.log(event);
+    console.log(event);
 
-      // const gameTiles = document.getElementsByTagName("game-tile");
-      // console.log('game tiles');
-      // console.log(gameTiles);
+    //document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].shadowRoot.querySelectorAll('.tile')[0].getAttribute('data-state')
+    //'correct'
 
-      // const tileDivs = document.getElementsByClassName("tile");
-      // console.log('tile divs');
-      // console.log(tileDivs);
+    // document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].getAttribute('evaluation')
+    // 'correct'
+    // document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].getAttribute('letter')
+    // 'a'
 
-      //document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].shadowRoot.querySelectorAll('.tile')[0].getAttribute('data-state')
-      //'correct'
+    // rows=document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')
 
-      // document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].getAttribute('evaluation')
-      // 'correct'
-      // document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].getAttribute('letter')
-      // 'a'
+    if (event.key === 'Enter') {
+      const gameRows = document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row');
+      console.log('rows length: ' + gameRows.length);
+      gameRows.forEach((gameRow, rowIndex) => {
 
-      // rows=document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')
+        const letters = gameRow.getAttribute('letters');
+        console.log('letters: ' + letters);
 
-      if (event.key === 'Enter') {
-        const gameRows = document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row');
-        console.log('rows length: ' + gameRows.length);
-        gameRows.forEach((gameRow, rowIndex) => {
+        const gameTiles = gameRow.shadowRoot.querySelectorAll('game-tile');
 
-          const letters = gameRow.getAttribute('letters');
-          console.log('letters: ' + letters);
-          
-          const gameTiles = gameRow.shadowRoot.querySelectorAll('game-tile');
-
-          gameTiles.forEach((gameTile, gameTileIndex) => {
-            const evaluation = gameTile.getAttribute('evaluation')
-            console.log('evaluation: ', evaluation);
-          });
+        gameTiles.forEach((gameTile, gameTileIndex) => {
+          const evaluation = gameTile.getAttribute('evaluation')
+          console.log('evaluation: ', evaluation);
         });
+      });
+    }
+
+    const key = event.key.toUpperCase();
+    if (key.length === 1) {
+
+      const isLetter = (key >= 'A' && key <= 'Z');
+
+      if (isLetter) {
+        const keyDownMessage = { keyDown: key };
+        console.log('send: ', keyDownMessage);
+        chrome.runtime.sendMessage(keyDownMessage);
       }
-
-      const key = event.key.toUpperCase();
-      if (key.length === 1) {
-
-        const isLetter = (key >= 'A' && key <= 'Z');
-
-        if (isLetter) {
-          const keyDownMessage = { keyDown: key };
-          console.log('send: ', keyDownMessage);
-          chrome.runtime.sendMessage(keyDownMessage);
-        }
-        event.preventDefault();
-      }
-    });
+      event.preventDefault();
+    }
   });
-}
 
-// http://localhost:8000
+}
