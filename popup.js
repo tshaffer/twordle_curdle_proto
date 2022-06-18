30c7167688301c14be94afe652f592ded2692081
Ted Shaffer <shaffer.family@gmail.com>
2022-06-18 15:56:43 -0700

    delay one second after Enter key is struck to process the DOM. Some cleanup as well.

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
 const serverUrl = 'http://localhost:8000';
 const versionUrl = serverUrl + '/api/v1/version';
 
-fetch(versionUrl)
-  .then(response => response.text())
-  .then(response => console.log(response));
+getVersion(versionUrl);
 
 chrome.tabs.query({ active: true, currentWindow: true })
   .then(([tab]) => {
@@ -23,11 +21,50 @@ chrome.tabs.query({ active: true, currentWindow: true })
   }
   );
 
+function getVersion(versionUrl) {
+  fetch(versionUrl)
+    .then(response => response.text())
+    .then(response => console.log(response));
+}
+
 // CONTENT SCRIPT
 function executeContentScript() {
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
-  // https://stackoverflow.com/questions/2257070/detect-numbers-or-letters-with-jquery-javascript
+  function processEnteredLines() {
+
+    console.log('processEnteredLines invoked');
+
+    const enteredLines = [];
+
+    const gameRows = document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row');
+    console.log('rows length: ' + gameRows.length);
+
+    gameRows.forEach((gameRow, rowIndex) => {
+
+      const letters = gameRow.getAttribute('letters');
+      console.log('letters: ' + letters);
+
+      const enteredLine = {
+        letters,
+        evaluations: []
+      };
+
+      const gameTiles = gameRow.shadowRoot.querySelectorAll('game-tile');
+
+      gameTiles.forEach((gameTile, gameTileIndex) => {
+        const evaluation = gameTile.getAttribute('evaluation')
+        console.log('evaluation: ', evaluation);
+        enteredLine.evaluations.push(evaluation);
+      });
+
+      enteredLines.push(enteredLine);
+
+    });
+
+    console.log('Entered lines');
+    console.log(enteredLines);
+
+  }
 
   document.body.addEventListener("keydown", async (event) => {
 
@@ -35,45 +72,24 @@ function executeContentScript() {
 
     console.log(event);
 
-    //document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].shadowRoot.querySelectorAll('.tile')[0].getAttribute('data-state')
-    //'correct'
-
-    // document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].getAttribute('evaluation')
-    // 'correct'
-    // document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')[0].shadowRoot.querySelectorAll('game-tile')[0].getAttribute('letter')
-    // 'a'
-
-    // rows=document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row')
-
+    // TEDTODO - don't fail if not on Wordle page
     if (event.key === 'Enter') {
-      const gameRows = document.querySelectorAll('game-app')[0].shadowRoot.querySelectorAll('#game')[0].querySelectorAll('game-row');
-      console.log('rows length: ' + gameRows.length);
-      gameRows.forEach((gameRow, rowIndex) => {
-
-        const letters = gameRow.getAttribute('letters');
-        console.log('letters: ' + letters);
-
-        const gameTiles = gameRow.shadowRoot.querySelectorAll('game-tile');
-
-        gameTiles.forEach((gameTile, gameTileIndex) => {
-          const evaluation = gameTile.getAttribute('evaluation')
-          console.log('evaluation: ', evaluation);
-        });
-      });
+      setTimeout(() => {
+        processEnteredLines();
+      }, 1000);
     }
-
-    const key = event.key.toUpperCase();
-    if (key.length === 1) {
-
-      const isLetter = (key >= 'A' && key <= 'Z');
-
-      if (isLetter) {
-        const keyDownMessage = { keyDown: key };
-        console.log('send: ', keyDownMessage);
-        chrome.runtime.sendMessage(keyDownMessage);
-      }
-      event.preventDefault();
-    }
+    // } else {
+    //   const key = event.key.toUpperCase();
+    //   if (key.length === 1) {
+    //     if (key >= 'A' && key <= 'Z') {
+    //       const keyDownMessage = { keyDown: key };
+    //       console.log('send: ', keyDownMessage);
+    //       chrome.runtime.sendMessage(keyDownMessage);
+    //     }
+    //     event.preventDefault();
+    //   }
+    // }
   });
-
 }
+
+
