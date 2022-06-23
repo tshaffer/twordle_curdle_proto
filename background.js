9d8425fe8d1c9abe2ae5fc78bf19ed1989f30d2d
Ted Shaffer <shaffer.family@gmail.com>
2022-06-23 15:50:08 -0700

    some progress??

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 // background.js
+const serverUrl = 'http://localhost:8000';
+const versionUrl = serverUrl + '/api/v1/version';
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
@@ -6,6 +8,17 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.action.onClicked.addListener(function(activeTab){
   console.log('onClicked invoked - create tab');
-  var newURL = "twordle.html";
+  var newURL = "./twordle.html";
   chrome.tabs.create({ url: newURL });
+
+  console.log('invoke getVersion');
+  getVersion(versionUrl);
 });
+
+function getVersion(versionUrl) {
+  console.log('getVersion invoked');
+  fetch(versionUrl)
+    .then(response => response.text())
+    .then(response => console.log(response));
+}
+
